5baa040d37d462e587f9474eda37a96aba40d90f
jesse-dahl <jddahl199@gmail.com>
2020-01-09 18:08:26 -0700

    added more barebones ejs files

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const parser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
 
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(parser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.render("home")
 });
 
 app.listen(3000, function () {

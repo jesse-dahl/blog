f57fb398cee5ffc708cecc2b7c99c405b5531d8b
jesse-dahl <jddahl199@gmail.com>
2020-01-15 09:26:54 -0700

    linked all necessary get and post routes with the database, finishing this small project

--- app.js
+++ app.js
@@ -25,16 +25,21 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts = [];
-
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(parser.urlencoded({ extended: true }));
 
 
-
 app.get("/", function (req, res) {
-  res.render("home", { posts: posts });
+  Post.find({}, function (err, posts) {
+    //Write a forEach loop that can print out each title
+    if (err) {
+      console.log(err);
+    }
+    res.render("home", {
+      posts: posts
+    });
+  });
 });
 
 app.get("/about", function (req, res) {
@@ -71,16 +76,14 @@ app.post("/compose", function (req, res) {
   const newPost = new Post({
     title: titleContent,
     body: bodyContent
-  })
-
-  newPost.save();
-
-  Post.find({}, function (err, posts) {
-    //Write a forEach loop that can print out each title
-    console.log(posts.title);
   });
 
-  res.redirect("/");
+  newPost.save(function (err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
+
 });
 
 app.listen(PORT, function () {

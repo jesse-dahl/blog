93d8d7d0089d5686c8607392f25c9d0f876a5ca0
jesse-dahl <jddahl199@gmail.com>
2020-01-16 22:05:57 -0700

    Added procfile

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const parser = require("body-parser");
 const ejs = require("ejs");
-const PORT = 3000;
 const mongoose = require('mongoose');
 const _ = require('lodash');
+const app = express();
 
 mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -18,8 +18,6 @@ const demo = new Post({
   title: "This is a test title",
   body: "This is a test body"
 });
-
-const app = express();
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -86,6 +84,8 @@ app.post("/compose", function (req, res) {
 
 });
 
-app.listen(PORT, function () {
-  console.log("Listening on port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
